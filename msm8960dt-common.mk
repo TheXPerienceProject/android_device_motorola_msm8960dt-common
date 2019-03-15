2b6c883595c0cf43163a586f2542b94e83251e20
klozz <shaclone707@gmail.com>
2019-03-15 03:41:02 -0400

    msm8960dt-common: Use build variables to build device matrices.
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8960dt-common.mk
+++ msm8960dt-common.mk
@@ -143,10 +143,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
